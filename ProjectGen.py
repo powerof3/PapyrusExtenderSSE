fd9f8b3f55ff5f09362eecc9822247a637904c36
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-01-16 04:12:11 +0530

    sort files in `ProjectGen.py`

--- ProjectGen.py
+++ ProjectGen.py
@@ -22,6 +22,8 @@ def make_cmake():
 			headers.append(name)
 		elif name.endswith(SOURCE_TYPES):
 			sources.append(name)
+	headers.sort()
+	sources.sort()
 
 	def do_make(a_filename, a_varname, a_files):
 		out = open("cmake/" + a_filename + ".cmake", "w", encoding="utf-8")
--- cmake/headerlist.cmake
+++ cmake/headerlist.cmake
@@ -1,49 +1,49 @@
 set(headers ${headers}
 	src/C++/Common.h
-	src/C++/PCH.h
 	src/C++/Game/Cache.h
-	src/C++/Game/Manager.h
 	src/C++/Game/EventHandler.h
-	src/C++/Serialization/Manager.h
-	src/C++/Serialization/Services.h
-	src/C++/Serialization/EventHolder.h
-	src/C++/Serialization/Registration/DataSet.h
-	src/C++/Serialization/Registration/DataMap.h
+	src/C++/Game/Manager.h
+	src/C++/PCH.h
+	src/C++/Papyrus/Functions/ActiveMagicEffect.h
+	src/C++/Papyrus/Functions/Actor.h
+	src/C++/Papyrus/Functions/ActorBase.h
+	src/C++/Papyrus/Functions/Alias.h
+	src/C++/Papyrus/Functions/ArmorAddon.h
+	src/C++/Papyrus/Functions/Array.h
+	src/C++/Papyrus/Functions/Book.h
+	src/C++/Papyrus/Functions/Cell.h
+	src/C++/Papyrus/Functions/Debug.h
+	src/C++/Papyrus/Functions/EffectShader.h
+	src/C++/Papyrus/Functions/Enchantment.h
+	src/C++/Papyrus/Functions/FEC.h
+	src/C++/Papyrus/Functions/Form.h
+	src/C++/Papyrus/Functions/Furniture.h
+	src/C++/Papyrus/Functions/Game.h
+	src/C++/Papyrus/Functions/Hazard.h
+	src/C++/Papyrus/Functions/Light.h
+	src/C++/Papyrus/Functions/Location.h
+	src/C++/Papyrus/Functions/MagicEffect.h
+	src/C++/Papyrus/Functions/ObjectReference.h
+	src/C++/Papyrus/Functions/Package.h
+	src/C++/Papyrus/Functions/Potion.h
+	src/C++/Papyrus/Functions/Projectile.h
+	src/C++/Papyrus/Functions/Scroll.h
+	src/C++/Papyrus/Functions/Sound.h
+	src/C++/Papyrus/Functions/Spell.h
+	src/C++/Papyrus/Functions/Strings.h
+	src/C++/Papyrus/Functions/UI.h
+	src/C++/Papyrus/Functions/Utility.h
+	src/C++/Papyrus/Functions/VisualEffect.h
+	src/C++/Papyrus/Functions/Weather.h
 	src/C++/Papyrus/Manager.h
 	src/C++/Papyrus/ObjectTypes.h
-	src/C++/Papyrus/Functions/ActorBase.h
-	src/C++/Papyrus/Functions/Book.h
-	src/C++/Papyrus/Functions/Enchantment.h
-	src/C++/Papyrus/Functions/Hazard.h
-	src/C++/Papyrus/Functions/Sound.h
-	src/C++/Papyrus/Functions/MagicEffect.h
-	src/C++/Papyrus/Functions/Strings.h
-	src/C++/Papyrus/Functions/Actor.h
-	src/C++/Papyrus/Functions/Potion.h
-	src/C++/Papyrus/Functions/Game.h
-	src/C++/Papyrus/Functions/Cell.h
-	src/C++/Papyrus/Functions/ObjectReference.h
-	src/C++/Papyrus/Functions/EffectShader.h
-	src/C++/Papyrus/Functions/FEC.h
-	src/C++/Papyrus/Functions/Spell.h
-	src/C++/Papyrus/Functions/Alias.h
-	src/C++/Papyrus/Functions/Package.h
-	src/C++/Papyrus/Functions/Location.h
-	src/C++/Papyrus/Functions/VisualEffect.h
-	src/C++/Papyrus/Functions/Utility.h
-	src/C++/Papyrus/Functions/Array.h
-	src/C++/Papyrus/Functions/Form.h
-	src/C++/Papyrus/Functions/Projectile.h
-	src/C++/Papyrus/Functions/ArmorAddon.h
-	src/C++/Papyrus/Functions/UI.h
-	src/C++/Papyrus/Functions/ActiveMagicEffect.h
-	src/C++/Papyrus/Functions/Scroll.h
-	src/C++/Papyrus/Functions/Debug.h
-	src/C++/Papyrus/Functions/Light.h
-	src/C++/Papyrus/Functions/Furniture.h
-	src/C++/Papyrus/Functions/Weather.h
+	src/C++/Papyrus/Util/ConditionParser.h
 	src/C++/Papyrus/Util/Graphics.h
 	src/C++/Papyrus/Util/Magic.h
-	src/C++/Papyrus/Util/ConditionParser.h
 	src/C++/Papyrus/Util/Script.h
+	src/C++/Serialization/EventHolder.h
+	src/C++/Serialization/Manager.h
+	src/C++/Serialization/Registration/DataMap.h
+	src/C++/Serialization/Registration/DataSet.h
+	src/C++/Serialization/Services.h
 )

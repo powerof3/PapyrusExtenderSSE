51d4cfeae3894c5f905ec085bb3dcec249b55f7b
powerof3 <powerof3@users.noreply.github.com>
2022-05-06 17:03:01 +0000

    maintenance

--- include/Papyrus/Functions/Actor.h
+++ include/Papyrus/Functions/Actor.h
@@ -369,12 +369,12 @@ namespace Papyrus::Actor
 
 		const auto actorValueList = RE::ActorValueList::GetSingleton();
 		const auto actorValue = actorValueList ?
-		                            actorValueList->LookupActorValueByName(a_actorValue) :
+                                    actorValueList->LookupActorValueByName(a_actorValue) :
                                     RE::ActorValue::kNone;
 
 		const auto modifier = static_cast<RE::ACTOR_VALUE_MODIFIER>(a_modifier);
 		return actorValue != RE::ActorValue::kNone ?
-		           a_actor->GetActorValueModifier(modifier, actorValue) :
+                   a_actor->GetActorValueModifier(modifier, actorValue) :
                    0.0f;
 	}
 
@@ -1462,7 +1462,7 @@ namespace Papyrus::Actor
 			charController->SetLinearVelocityImpl(0.0f);
 
 			a_disableGravityOnGround ?
-				charController->flags.reset(RE::CHARACTER_FLAGS::kNoGravityOnGround) :
+                charController->flags.reset(RE::CHARACTER_FLAGS::kNoGravityOnGround) :
                 charController->flags.set(RE::CHARACTER_FLAGS::kNoGravityOnGround);
 
 			charController->gravity = a_value;

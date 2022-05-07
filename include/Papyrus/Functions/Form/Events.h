f7673c35181694afcb6b1abc53a28248eddf21b2
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-05-07 09:43:25 +0530

    Add `TESForm` versions of unique events

--- include/Papyrus/Functions/Form/Events.h
+++ include/Papyrus/Functions/Form/Events.h
@@ -4,6 +4,17 @@
 
 namespace Papyrus::Form::Events
 {
+	inline void RegisterForActorFallLongDistance(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorFallLongDistance;
+		regs.Register(a_form);
+	}
+
 	inline void RegisterForActorKilled(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, const RE::TESForm* a_form)
 	{
 		if (!a_form) {
@@ -12,6 +23,39 @@ namespace Papyrus::Form::Events
 		}
 
 		auto& regs = Event::StoryEventHolder::GetSingleton()->actorKill;
+		regs.Register(a_form);
+	}
+
+	inline void RegisterForActorReanimateStart(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorReanimateStart;
+		regs.Register(a_form);
+	}
+
+	inline void RegisterForActorReanimateStop(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorReanimateStop;
+		regs.Register(a_form);
+	}
+
+	inline void RegisterForActorResurrected(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorResurrect;
 		regs.Register(a_form);
 	}
 
@@ -70,6 +114,25 @@ namespace Papyrus::Form::Events
 		regs.Register(a_form);
 	}
 
+	inline void RegisterForHitEventEx(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form,
+		RE::TESForm* a_aggressorFilter,
+		RE::TESForm* a_sourceFilter,
+		RE::TESForm* a_projectileFilter,
+		std::int32_t a_powerFilter,
+		std::int32_t a_sneakFilter,
+		std::int32_t a_bashFilter,
+		std::int32_t a_blockFilter,
+		bool a_match)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->onHit;
+		regs.Register(a_form, { a_aggressorFilter, a_sourceFilter, a_projectileFilter, a_powerFilter, a_sneakFilter, a_bashFilter, a_blockFilter }, a_match);
+	}
+
 	inline void RegisterForItemCrafted(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, const RE::TESForm* a_form)
 	{
 		if (!a_form) {
@@ -114,6 +177,31 @@ namespace Papyrus::Form::Events
 		regs.Register(a_form);
 	}
 
+	inline void RegisterForMagicEffectApplyEx(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*,
+		RE::TESForm* a_form,
+		RE::TESForm* a_effectFilter,
+		bool a_match)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->magicApply;
+		regs.Register(a_form, a_effectFilter, a_match);
+	}
+
+	inline void RegisterForMagicHit(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->magicHit;
+		regs.Register(a_form);
+	}
+
 	inline void RegisterForObjectGrab(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, const RE::TESForm* a_form)
 	{
 		if (!a_form) {
@@ -144,6 +232,17 @@ namespace Papyrus::Form::Events
 
 		auto& unload = Event::ScriptEventHolder::GetSingleton()->objectUnloaded;
 		unload.Register(a_form, formType);
+	}
+
+	inline void RegisterForProjectileHit(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->projectileHit;
+		regs.Register(a_form);
 	}
 
 	inline void RegisterForQuest(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, const RE::TESForm* a_form, RE::TESQuest* a_quest)
@@ -236,6 +335,28 @@ namespace Papyrus::Form::Events
 		regs.Register(a_form);
 	}
 
+	inline void RegisterForWeaponHit(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->weaponHit;
+		regs.Register(a_form);
+	}
+
+	inline void UnregisterForActorFallLongDistance(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorFallLongDistance;
+		regs.Unregister(a_form);
+	}
+
 	inline void UnregisterForActorKilled(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, const RE::TESForm* a_form)
 	{
 		if (!a_form) {
@@ -244,6 +365,39 @@ namespace Papyrus::Form::Events
 		}
 
 		auto& regs = Event::StoryEventHolder::GetSingleton()->actorKill;
+		regs.Unregister(a_form);
+	}
+
+	inline void UnregisterForActorReanimateStart(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorReanimateStart;
+		regs.Unregister(a_form);
+	}
+
+	inline void UnregisterForActorReanimateStop(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorReanimateStop;
+		regs.Unregister(a_form);
+	}
+
+	inline void UnregisterForActorResurrected(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->actorResurrect;
 		regs.Unregister(a_form);
 	}
 
@@ -302,6 +456,37 @@ namespace Papyrus::Form::Events
 		regs.Unregister(a_form);
 	}
 
+	inline void UnregisterForHitEventEx(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*,
+		RE::TESForm* a_form,
+		RE::TESForm* a_aggressorFilter,
+		RE::TESForm* a_sourceFilter,
+		RE::TESForm* a_projectileFilter,
+		std::int32_t a_powerFilter,
+		std::int32_t a_sneakFilter,
+		std::int32_t a_bashFilter,
+		std::int32_t a_blockFilter,
+		bool a_match)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->onHit;
+		regs.Unregister(a_form, { a_aggressorFilter, a_sourceFilter, a_projectileFilter, a_powerFilter, a_sneakFilter, a_bashFilter, a_blockFilter }, a_match);
+	}
+
+	inline void UnregisterForAllHitEventsEx(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->onHit;
+		regs.UnregisterAll(a_form);
+	}
+
 	inline void UnregisterForItemCrafted(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, const RE::TESForm* a_form)
 	{
 		if (!a_form) {
@@ -343,6 +528,42 @@ namespace Papyrus::Form::Events
 		}
 
 		auto& regs = Event::StoryEventHolder::GetSingleton()->locationDiscovery;
+		regs.Unregister(a_form);
+	}
+
+	inline void UnregisterForMagicEffectApplyEx(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*,
+		RE::TESForm* a_form,
+		RE::TESForm* a_effectFilter,
+		bool a_match)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->magicApply;
+		regs.Unregister(a_form, a_effectFilter, a_match);
+	}
+
+	inline void UnregisterForAllMagicEffectApplyEx(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->magicApply;
+		regs.UnregisterAll(a_form);
+	}
+
+	inline void UnregisterForMagicHit(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->magicHit;
 		regs.Unregister(a_form);
 	}
 
@@ -390,6 +611,17 @@ namespace Papyrus::Form::Events
 
 		auto& unload = Event::ScriptEventHolder::GetSingleton()->objectUnloaded;
 		unload.UnregisterAll(a_form);
+	}
+
+	inline void UnregisterForProjectileHit(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->projectileHit;
+		regs.Unregister(a_form);
 	}
 
 	inline void UnregisterForQuest(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*,
@@ -509,43 +741,73 @@ namespace Papyrus::Form::Events
 		regs.Unregister(a_form);
 	}
 
+	inline void UnregisterForWeaponHit(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm->TraceStack("Form is None", a_stackID);
+			return;
+		}
+
+		auto& regs = Event::GameEventHolder::GetSingleton()->weaponHit;
+		regs.Unregister(a_form);
+	}
+
 	inline void Bind(VM& a_vm)
 	{
 		auto constexpr obj = "PO3_Events_Form"sv;
 
+		BIND_EVENT(RegisterForActorFallLongDistance, true);
 		BIND_EVENT(RegisterForActorKilled, true);
+		BIND_EVENT(RegisterForActorReanimateStart, true);
+		BIND_EVENT(RegisterForActorReanimateStop, true);
+		BIND_EVENT(RegisterForActorResurrected, true);
 		BIND_EVENT(RegisterForBookRead, true);
 		BIND_EVENT(RegisterForCellFullyLoaded, true);
 		BIND_EVENT(RegisterForCriticalHit, true);
 		BIND_EVENT(RegisterForDisarmed, true);
 		BIND_EVENT(RegisterForDragonSoulGained, true);
+		BIND_EVENT(RegisterForHitEventEx, true);
 		BIND_EVENT(RegisterForItemCrafted, true);
 		BIND_EVENT(RegisterForItemHarvested, true);
 		BIND_EVENT(RegisterForLevelIncrease, true);
 		BIND_EVENT(RegisterForLocationDiscovery, true);
+		BIND_EVENT(RegisterForMagicEffectApplyEx, true);
+		BIND_EVENT(RegisterForMagicHit, true);
 		BIND_EVENT(RegisterForObjectGrab, true);
 		BIND_EVENT(RegisterForObjectLoaded, true);
 		BIND_EVENT(RegisterForQuest, true);
+		BIND_EVENT(RegisterForProjectileHit, true);
 		BIND_EVENT(RegisterForQuestStage, true);
 		BIND_EVENT(RegisterForShoutAttack, true);
 		BIND_EVENT(RegisterForSkillIncrease, true);
 		BIND_EVENT(RegisterForSoulTrapped, true);
 		BIND_EVENT(RegisterForSpellLearned, true);
 		BIND_EVENT(RegisterForWeatherChange, true);
+		BIND_EVENT(RegisterForWeaponHit, true);
 
+		BIND_EVENT(UnregisterForActorFallLongDistance, true);
 		BIND_EVENT(UnregisterForActorKilled, true);
+		BIND_EVENT(UnregisterForActorReanimateStart, true);
+		BIND_EVENT(UnregisterForActorReanimateStop, true);
+		BIND_EVENT(UnregisterForActorResurrected, true);
 		BIND_EVENT(UnregisterForBookRead, true);
 		BIND_EVENT(UnregisterForCellFullyLoaded, true);
 		BIND_EVENT(UnregisterForCriticalHit, true);
 		BIND_EVENT(UnregisterForDisarmed, true);
 		BIND_EVENT(UnregisterForDragonSoulGained, true);
+		BIND_EVENT(UnregisterForHitEventEx, true);
+		BIND_EVENT(UnregisterForAllHitEventsEx, true);
 		BIND_EVENT(UnregisterForItemCrafted, true);
 		BIND_EVENT(UnregisterForItemHarvested, true);
 		BIND_EVENT(UnregisterForLevelIncrease, true);
 		BIND_EVENT(UnregisterForLocationDiscovery, true);
+		BIND_EVENT(UnregisterForMagicEffectApplyEx, true);
+		BIND_EVENT(UnregisterForAllMagicEffectApplyEx, true);
+		BIND_EVENT(UnregisterForMagicHit, true);
 		BIND_EVENT(UnregisterForObjectGrab, true);
 		BIND_EVENT(UnregisterForObjectLoaded, true);
 		BIND_EVENT(UnregisterForAllObjectsLoaded, true);
+		BIND_EVENT(UnregisterForProjectileHit, true);
 		BIND_EVENT(UnregisterForQuest, true);
 		BIND_EVENT(UnregisterForAllQuests, true);
 		BIND_EVENT(UnregisterForQuestStage, true);
@@ -555,6 +817,7 @@ namespace Papyrus::Form::Events
 		BIND_EVENT(UnregisterForSoulTrapped, true);
 		BIND_EVENT(UnregisterForSpellLearned, true);
 		BIND_EVENT(UnregisterForWeatherChange, true);
+		BIND_EVENT(UnregisterForWeaponHit, true);
 
 		logger::info("Registered form events"sv);
 	}

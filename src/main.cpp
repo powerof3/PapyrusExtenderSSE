a22bda426255d8d7a3bacc60cd2bd3625daff3b3
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-02-07 12:26:03 +0530

    Support 1.6.353-

--- src/main.cpp
+++ src/main.cpp
@@ -68,8 +68,7 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
 	v.PluginVersion(Version::MAJOR);
 	v.PluginName("powerofthree's Papyrus Extender");
 	v.AuthorName("powerofthree");
-	v.UsesAddressLibrary();
-	v.UsesUpdatedStructs();
+	v.UsesAddressLibrary(true);
 	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
 
 	return v;

57dafd2fec561f36a7e8f3160a9d618d44c13a4e
powerof3 <powerof3@users.noreply.github.com>
2024-10-04 03:44:14 +0530

    Add `SetObjectiveText`

--- include/Papyrus/Functions/Quest.h
+++ include/Papyrus/Functions/Quest.h
@@ -18,9 +18,24 @@ namespace Papyrus::Quest
 		return indices;
 	}
 
+	inline void SetObjectiveText(STATIC_ARGS, RE::TESQuest* a_quest, RE::BSFixedString a_text, std::int32_t a_index)
+	{
+		if (!a_quest) {
+			a_vm->TraceStack("Quest is None", a_stackID);
+		}
+
+		for (const auto& objective : a_quest->objectives) {
+			if (objective && objective->index == a_index) {
+				objective->displayText = a_text;
+				break;
+			}
+		}
+	}
+
 	inline void Bind(VM& a_vm)
 	{
 		BIND(GetAllQuestObjectives);
+		BIND(SetObjectiveText);
 
 		logger::info("Registered quest functions"sv);
 	}
